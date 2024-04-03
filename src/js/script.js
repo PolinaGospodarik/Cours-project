8e72b6a946f1be682fc1248d7df7b3247c5800b6
PolinaGospodarik <polinagos03@gmail.com>
2024-04-03 03:25:28 +0300

    fix bugs, adding a burger

--- src/js/script.js
+++ src/js/script.js
@@ -6,13 +6,45 @@ window.$ = $;
 // Импорт Slick Slider
 import 'slick-carousel'; 
 
-
 $('.slider').slick({
     dots: true,
-    infinite: true,
     speed: 500,
     slidesToShow: 1,
     arrows: false,
-    autoplay:true,
-    autoplaySpeed:3000
+    // autoplay:true,
+    autoplaySpeed:3000,
+    pauseOnDotsHover:true,
+    pauseOnFocus: true, 
+    pauseOnDotsHover: true
 });
+
+
+// $(function() {
+//     $('.menu__burger').on('click', function() {
+//        $('.menu__burger, .menu__list').toggleClass('active');
+//     });
+//  });
+
+ $(function() {
+    $('.menu__burger').on('click', function() {
+        // Переключение видимости иконок
+        $('.burger-icon').toggle();
+        $('.close-icon').toggle();
+
+        // Переключение класса 'active' для меню
+        $('.menu__burger, .menu__list').toggleClass('active');
+    });
+});
+
+
+
+// const slides = document.querySelectorAll('.slider-item');
+
+// // Проходимся по слайдам и задаем им чередующиеся классы
+// slides.forEach((slide, index) => {
+//     if (index % 2 === 0) {
+//         slide.classList.add('red-bg');
+//     } else {
+//         slide.classList.add('blue-bg');
+//     }
+// });
