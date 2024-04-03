59287506c1b035d3cdd7276e957870de84df8d72
PolinaGospodarik <polinagos03@gmail.com>
2024-04-03 12:16:46 +0300

    fix bugs, adaptive slider, button animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
-// Импорт jQuery
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-// Импорт Slick Slider
 import 'slick-carousel'; 
 
 $('.slider').slick({
     dots: true,
+    infinite: true,
     speed: 500,
     slidesToShow: 1,
     arrows: false,
-    // autoplay:true,
+    autoplay:true,
     autoplaySpeed:3000,
     pauseOnDotsHover:true,
     pauseOnFocus: true, 
@@ -19,32 +18,12 @@ $('.slider').slick({
 });
 
 
-// $(function() {
-//     $('.menu__burger').on('click', function() {
-//        $('.menu__burger, .menu__list').toggleClass('active');
-//     });
-//  });
-
  $(function() {
     $('.menu__burger').on('click', function() {
-        // Переключение видимости иконок
+      
         $('.burger-icon').toggle();
         $('.close-icon').toggle();
 
-        // Переключение класса 'active' для меню
         $('.menu__burger, .menu__list').toggleClass('active');
     });
 });
-
-
-
-// const slides = document.querySelectorAll('.slider-item');
-
-// // Проходимся по слайдам и задаем им чередующиеся классы
-// slides.forEach((slide, index) => {
-//     if (index % 2 === 0) {
-//         slide.classList.add('red-bg');
-//     } else {
-//         slide.classList.add('blue-bg');
-//     }
-// });
